090deae75dcb3da9c3886eccfa8b68bff0091cc3
Shreyash-Malusare <157025625+Shreyash-Malusare@users.noreply.github.com>
2025-03-15 18:19:18 +0530

    Update products.ts

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const VITE_FRONT_END_IP = import.meta.env.VITE_FRONT_END_IP;
 
-const API_URL = `${VITE_FRONT_END_IP}:3001/api/products`;
+const API_URL = `${VITE_FRONT_END_IP}/api/products`;
 
 export const fetchProducts = async (
   category: string,
